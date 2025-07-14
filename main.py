c111b864680bd8cdc6902cb4fa5a561016458098
F1l14 <filipmn42@gmail.com>
2025-07-14 11:57:52 +0300

    Update: functions for eclass and progress handling

--- main.py
+++ main.py
@@ -3,21 +3,33 @@ from progress import progressConnector
 from user import User
 from ocr import OCR
 import os
+
+def run_progress():
+    progress = progressConnector(username, password, headless=False)
+    progress.login()
+    progress.fetch_captcha_image()
+    ocr = OCR()
+    ocr.preprocess("temp/captcha.png")
+    result = ocr.recognise_text("output/processed.png")
+    print("main result:", result)
+    os.remove("temp/captcha.png")
+    progress.verify_captcha(result)
+    progress.get_grades()
+
+def run_eclass():
+    eclass = EclassConnector(username, password, headless=False)
+    eclass.login()
+    # eclass.fetch_courses()
+    eclass.fetch_course_content("CEID1159")  # Example usage to fetch course content
+    # eclass.fetch_course_content("course_id")  # Example usage to fetch course content
+
+
+
 user = User()
 username, password = user.login()
-# eclass = EclassConnector(username, password, headless=False)
-# eclass.login()
-# eclass.fetch_courses()
 
-progress = progressConnector(username, password, headless=False)
-progress.login()
-progress.fetch_captcha_image()
-ocr = OCR()
-ocr.preprocess("temp/captcha.png")
-result = ocr.recognise_text("output/processed.png")
-print("main result:", result)
-os.remove("temp/captcha.png")
-progress.verify_captcha(result)
+run_eclass()
+
 input("Press Enter to exit...")
 
 
